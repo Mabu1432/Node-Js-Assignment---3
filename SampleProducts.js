fafc1fb0615deeb538c52f597984a5f2d5999d47
Mabu1432 <91015400+Mabu1432@users.noreply.github.com>
2021-11-29 23:21:20 +0530

    Update SampleProducts.js

--- SampleProducts.js
+++ SampleProducts.js
@@ -5,7 +5,7 @@ const isAdmin=true;
 
 
 router.get("/sampleProducts/getdata",(req,res)=>{
-    res.json([ {Product1 : "Bat"} , {  product2 : "ball" }   ] )})
+    res.json([ {Product1 : "Base"} , {  product2 : "ball" }   ] )})
 
 
 module.exports=router
